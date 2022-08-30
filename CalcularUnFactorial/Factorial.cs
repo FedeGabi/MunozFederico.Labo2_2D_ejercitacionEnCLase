739279cbc7d0b874db81a26c2b62db64bf3c7933
FedeGabi <federicomunoz1986@gmail.com>
2022-08-29 23:04:11 -0300

    ejercicios

---
+++ CalcularUnFactorial/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularUnFactorial
+{
+    internal static class Factorial
+    {
+
+        public static double CalcularFactorial(double numero)
+        {
+            if(numero==0)
+            {
+                return 1;
+            }
+            return numero * CalcularFactorial(numero -1);
+        }
+
+    }
+}
---
+++ CalcularUnFactorial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalcularUnFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Factorial.CalcularFactorial(7));
+        }
+    }
+}
